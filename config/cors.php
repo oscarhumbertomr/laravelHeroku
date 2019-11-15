926b77dbb8935e2c92c6857a1dc196350f02d551
oscar.montejo <oscarhumbertomr@gmail.com>
2019-11-14 22:44:17 -0600

    jws works

--- config/cors.php
+++ config/cors.php
@@ -12,12 +12,20 @@ return [
     |
     */
    
-    'supportsCredentials' => false,
+    'supportsCredentials' => true,
     'allowedOrigins' => ['*'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
     'exposedHeaders' => [],
-    'maxAge' => 0,
+    'maxAge' => 10000,
 
 ];
+
+/*
+ * header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+"GET", "PUT", "POST", "DELETE", "OPTIONS"
+header('Access-Control-Max-Age: 1000');*/
